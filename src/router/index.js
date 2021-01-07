6074877edc8183c927b122ff7796c40a8c34869c
lin_jianhe <jianhe_lin@163.com>
2021-01-07 23:31:45 +0800

    移动端初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+const routes = [{
+  path: '/',
+  name: 'Home',
+  redirect: '/home',
+  component: () => import('../views/Home.vue'),
+  children: [{
+    path: "/home",
+    name: "home",
+    component: () => import("../views/layout/home/index.vue")
+  }, {
+    path: "/discover",
+    name: "discover",
+    component: () => import("../views/layout/descover/index.vue")
+  }, {
+    path: "/cart",
+    name: "cart",
+    component: () => import("../views/layout/cart/index.vue")
+  }, {
+    path: "/mine",
+    name: "mine",
+    component: () => import("../views/layout/mine/index.vue")
+  }]
+}, ]
 
 const router = new VueRouter({
   mode: 'history',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="navBar">
+      <router-link
+        :to="item.path"
+        v-for="(item, index) in textBar"
+        :key="index"
+        >{{ item.text }}</router-link
+      >
+    </div>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      textBar: [
+        { text: "首页", path: "/home" },
+        { text: "发现", path: "/discover" },
+        { text: "购物车", path: "/cart" },
+        { text: "我的", path: "mine" },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.navBar {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  height: 70px;
+  border-top: 1px solid #ccc;
+  a {
+    flex: 1;
+    text-align: center;
+    font-size: 18px;
+    &.router-link-active {
+      color: rgb(2, 167, 243);
+    }
   }
 }
-</script>
+</style>
